58d3d0b1ca99e33b2b13122ed44bb4a95100af00
higain <maxi.stauss@t-online.de>
2016-05-04 16:00:56 +0200

    Update getCommunityEdition.sh

--- getCommunityEdition.sh
+++ getCommunityEdition.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --quiet http://dl.alfresco.com/release/community/201604-build-00007/alfresco-community-installer-201604-linux-x64.bin > AlfrescoCommunity.bin 
+wget --quiet http://dl.alfresco.com/release/community/201604-build-00007/alfresco-community-installer-201604-linux-x64.bin -O AlfrescoCommunity.bin 
